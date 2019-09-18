7c7c5869b1c70f5492a2c4cac4f980687db2a3a8
Yoohj <ssenssen24@gmail.com>
2019-09-18 15:42:12 +0900

    first commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,10 @@
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import 'bootstrap/scss/bootstrap.scss';
+import '../css/fontawesome-all.css';
+import '../scss/style.scss';
+// import 'lightbox2/dist/css/lightbox.min.css';
+// import 'lightbox2/dist/js/lightbox-plus-jquery.js';
+
+import {wow} from './modules/wowAnimate.js';
